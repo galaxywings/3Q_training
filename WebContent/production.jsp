4723d3607d903d21710bffa96ef1d9cad60b8eec
galaxywings <buster.wwy@gmail.com>
2014-09-21 21:42:37 +0800

    servlet & js

--- WebContent/production.jsp
+++ WebContent/production.jsp
@@ -7,26 +7,27 @@
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>商铺商品展示页</title>
 <link rel="stylesheet" href="resource/css/css3.css"/>
+<script src="resource/js/jquery-1.11.1.js" type="text/javascript" ></script>
 </head>
 <body>
      <nav  id="title_nav">
        <span id="title_span">展示商铺1</span>
      </nav>
-<div>
-<div class="layout">
- <form method="post" action="shopping">
+<section>
+<section class="layout">
+ <form id="goodsForm" method="post" action="shopping">
   <input name="nextaction" value="add" type="hidden" />
-
+  <input type="hidden" name="cart"  value="" />
   <div class="row">
-  
   <div class="goods">
     <div>
   	<img alt="农夫山泉" src="resource/img/water.jpg">
   	</div>
   	<div class="btns">
-  	 <a class="btn-buy">加入购物车</a>
+  	 <input type="hidden" value="1" name="proId"/>
+  	 <a class="btn-buy" onclick="buyToCart(this)">加入购物车</a>
   	 <span>农夫山泉&nbsp;&nbsp;&nbsp;￥ 24.9</span>
-  	 <a class="btn-buy" style="float:right">关注</a>
+  	 <a class="btn-buy" onclick="focusOn();"  style="float:right">关注</a>
   	</div>
   </div>
   
@@ -35,10 +36,10 @@
      <img alt="鼠标" src="resource/img/mouse.jpg">
      </div>
      <div class ="btns">
-      <a class="btn-buy">加入购物车</a>
+      <a class="btn-buy" onclick="buyToCart(this)">加入购物车</a>
       <input type="hidden" value="2" name="proId"/>
       <span>鼠标&nbsp;&nbsp;&nbsp;￥20.0</span>
-       <a class="btn-buy" style="float:right">关注</a>
+       <a class="btn-buy" onclick="focusOn();" style="float:right">关注</a>
      </div>
   </div>
   
@@ -47,10 +48,10 @@
      <img alt="笔记本" src="resource/img/laptop.jpg">
     </div>
     <div  class ="btns">
-      <a class="btn-buy">加入购物车</a>
-      <input type="hidden" value="3" name="proId"/>
+      <a class="btn-buy" onclick="buyToCart(this)">加入购物车</a>
       <span>笔记本&nbsp;&nbsp;&nbsp;￥3599.0</span>
-       <a class="btn-buy" style="float:right">关注</a>
+       <input type="hidden" value="3" name="proId"/>
+       <a class="btn-buy" onclick="focusOn();" style="float:right">关注</a>
     </div>
   </div>
   
@@ -63,9 +64,10 @@
   	<img alt="洗衣机" src="resource/img/xyj.jpg">
   	</div>
   	<div class="btns">
-  	 <a class="btn-buy">加入购物车</a>
+  	<input type="hidden" value="4" name="proId"/>
+  	 <a class="btn-buy"  onclick="buyToCart(this)">加入购物车</a>
   	 <span>洗衣机&nbsp;&nbsp;&nbsp;￥ 4000</span>
-  	 <a class="btn-buy" style="float:right">关注</a>
+  	 <a class="btn-buy" onclick="focusOn();" style="float:right">关注</a>
   	</div>
   </div>
   
@@ -74,10 +76,10 @@
      <img alt="饮水机" src="resource/img/ysj.jpg">
      </div>
      <div class ="btns">
-      <a class="btn-buy">加入购物车</a>
-      <input type="hidden" value="2" name="proId"/>
+      <a class="btn-buy" onclick="buyToCart(this)">加入购物车</a>
+      <input type="hidden" value="5" name="proId"/>
       <span>饮水机&nbsp;&nbsp;&nbsp;￥280.0</span>
-       <a class="btn-buy" style="float:right">关注</a>
+       <a class="btn-buy" onclick="focusOn();" style="float:right">关注</a>
      </div>
   </div>
   
@@ -86,17 +88,18 @@
      <img alt="电话机" src="resource/img/phone.jpg">
     </div>
     <div  class ="btns">
-      <a class="btn-buy">加入购物车</a>
-      <input type="hidden" value="3" name="proId"/>
+      <a class="btn-buy" onclick="buyToCart(this)">加入购物车</a>
+      <input type="hidden" value="6" name="proId"/>
       <span>电话机&nbsp;&nbsp;&nbsp;￥78.0</span>
-       <a class="btn-buy" style="float:right">关注</a>
+       <a class="btn-buy" onclick="focusOn();" style="float:right">关注</a>
     </div>
   </div>
   
   </div>
    </form>
- </div>
- <div class="login">
+ </section>
+ 
+ <section class="login">
  <form action="">
  <table>
    <tr>
@@ -106,20 +109,45 @@
      <td>密码</td><td><input type="password" value="" /></td>
    </tr>
    <tr><td colspan="2"><a class="btn-buy" style="width:80%">登录</a></td></tr>
+   <tr><td colspan="2"><a class="btn-buy" onclick="goCart();" style="width:80%">去购物车</a></td></tr>
  </table>
  </form>
  
    <div class="logo">
   	<img alt="农夫山泉" src="resource/img/logo1.jpg" />
    </div>
- </div>
+ </section>
  
-
- 
- </div>     
+ </section>  
  
 
   </body>
 <script type="text/javascript">
+function focusOn(){
+	alert("关注成功");
+}
+
+function buyToCart(obj){
+	var goodsId = $(obj).parent().find("input[name=proId]").val();
+	var cart = $("input[name=cart]").val();
+	var ifExist = cart.indexOf(goodsId);
+	if(ifExist!=-1){
+		alert("该物品已在购物车中！");
+	}else{
+	$("input[name=cart]").val(cart+goodsId+",")
+	   alert("已添加到购物车");
+	}
+}
+
+function goCart(){
+	var cart = $("input[name=cart]").val();
+	if(cart==""){
+		alert("请选择商品！");
+	}else{
+		$("#goodsForm").submit();
+	}
+
+}
+
 </script>
 </html>
--- src/shopping/Shopping.java
+++ src/shopping/Shopping.java
@@ -1,6 +1,11 @@
 package shopping;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,14 +20,18 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class Shopping  extends HttpServlet {
+	
+	private final static String JDriver = "com.mysql.jdbc.Driver"; // MySQL提供的JDBC驱动，要保证它在CLASSPATH里可见
+	private final static String conURL = "jdbc:mysql://localhost:3306/3Q_training"; // 本地计算机上的MySQL数据库Company的URL
+	
 	   public void init()throws ServletException{      
 	        
-	        Map products = new HashMap();       
-	        products.put("1", new Product("1","农夫山泉","",2.0));          
-	        products.put("2", new Product("2","鼠标","",20.0));        
-	        products.put("3", new Product("3","笔记本","",8.0));
-	        ServletContext context = getServletContext();
-	        context.setAttribute("products", products);     
+//	        Map products = new HashMap();       
+//	        products.put("1", new Product("1","农夫山泉","",2.0));          
+//	        products.put("2", new Product("2","鼠标","",20.0));        
+//	        products.put("3", new Product("3","笔记本","",8.0));
+//	        ServletContext context = getServletContext();
+//	        context.setAttribute("products", products);     
 	    }    
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -33,34 +42,56 @@ public class Shopping  extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
 	        throws ServletException, IOException {
     	
+    	Map products = new HashMap();
     	
-    		
-	        
-    		String nextaction = request.getParameter("nextaction");
-	        String proIds[] = request.getParameterValues("proId");
-	        ServletContext context = getServletContext();
-	        Map products = (Map) context.getAttribute("products");
-	        
-	        Map shopCart = (Map)request.getSession().getAttribute("shopCart");
-	        if(shopCart == null){
-	            shopCart = new HashMap();
-	        }       
-	        if(nextaction.equals("add")&&proIds!=null){
-	            for(int i=0;i<proIds.length;i++){
-	                Product product = (Product)products.get(proIds[i]);             
-	                if(shopCart.get(proIds[i])==null){//the product isn't in shopCart
-	                	shopCart.put(proIds[i], product);
-	                }                
-	            }
-	        }else if(nextaction.equals("del")&&proIds!=null){
-	            for(int i=0;i<proIds.length;i++){
-	            	 shopCart.remove(proIds[i]);
-	            }
-	        }           
-	        
-	        request.getSession().setAttribute("shopCart", shopCart);
-	        RequestDispatcher dispatcher = context.getRequestDispatcher("/shoppingCart.jsp");
-	        dispatcher.forward(request, response);      
+		try {
+			Class.forName(JDriver);
+			Connection con = DriverManager.getConnection(conURL, "root","123456"); // 连接数据库
+			Statement s = con.createStatement(); // Statement类用来提交SQL语句
+			
+			
+			ResultSet rs = s.executeQuery("select * from t_goods;"); // 提交查询，返回的表格保存在rs中
+			while (rs.next()) { // ResultSet指针指向下一个“行”
+//				System.out.println(rs.getInt("Id") + "\t"
+//						+ rs.getString("goods_name")+ "\t"
+//								+ rs.getBigDecimal("value"));
+				products.put(rs.getString("Id"), new Product(rs.getString("Id"),rs.getString("goods_name"),"",rs.getDouble("value")));
+			}
+			
+			rs.close();
+			s.close();// 释放Statement对象
+			con.close(); // 关闭到MySQL服务器的连接
+		} catch (ClassNotFoundException cnf_e) { // 如果找不到驱动类
+			System.out.println("Driver Not Found: " + cnf_e);
+		} catch (SQLException sql_e) { // 都是SQLException
+			System.out.println(sql_e);
+		}
+		
+		String nextaction = request.getParameter("nextaction");
+        String car =request.getParameter("cart");
+        String[] proIds = car.split(",");
+        ServletContext context = getServletContext();
+        
+        Map shopCart = (Map)request.getSession().getAttribute("shopCart");
+        if(shopCart == null){
+            shopCart = new HashMap();
+        }       
+        if(nextaction.equals("add")&&proIds!=null){
+            for(int i=0;i<proIds.length;i++){
+                Product product = (Product)products.get(proIds[i]);             
+                if(shopCart.get(proIds[i])==null){//the product isn't in shopCart
+                	shopCart.put(proIds[i], product);
+                }                
+            }
+        }else if(nextaction.equals("del")&&proIds!=null){
+            for(int i=0;i<proIds.length;i++){
+            	 shopCart.remove(proIds[i]);
+            }
+        }           
+        
+        request.getSession().setAttribute("shopCart", shopCart);
+        RequestDispatcher dispatcher = context.getRequestDispatcher("/shoppingCart.jsp");
+        dispatcher.forward(request, response);      
 	    }
 	
 }
